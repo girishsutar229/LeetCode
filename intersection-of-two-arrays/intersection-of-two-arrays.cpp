ad30133130815fafa05c1be86a074cd7313a6d36
Girish Suthar <102278281+girishsutar229@users.noreply.github.com>
2023-06-18 17:39:34 +0530

    Time: 3 ms (93.91%) | Memory: 9.9 MB (92.61%) - LeetSync

--- intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -2,13 +2,15 @@ class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
         vector<int>ans;
+        int m=nums1.size();
+        int n=nums2.size();
    
         sort(nums1.begin(),nums1.end());
         sort(nums2.begin(),nums2.end());
 
         int i=0;
         int j=0;
-        while( i<nums1.size() && j<nums2.size() ){
+        while( i<m && j<n ){
             
             if(i > 0 && nums1[i]==nums1[i-1]){ //this will help to remove the duplicate entries
                 i++;
