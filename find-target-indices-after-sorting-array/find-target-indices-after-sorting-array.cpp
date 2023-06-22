0db1348ac63c18ad08671302eceedc9e3e84548b
Girish Suthar <102278281+girishsutar229@users.noreply.github.com>
2023-06-22 10:29:40 +0530

    Time: 4 ms (86.48%) | Memory: 11.7 MB (32.96%) - LeetSync

--- find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.cpp
+++ find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     vector<int> targetIndices(vector<int>& nums, int target) {
-        vector<int> ans;
+        vector<int> answer;
 
         sort(nums.begin(),nums.end());
 
         for(int i=0;i<nums.size();i++){
             if(nums[i]==target){
-                ans.push_back(i);
+                answer.push_back(i);
             }
         }
-        return ans;
+        return answer;
         
     }
 };
