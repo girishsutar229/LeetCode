8082037bc93f628103eb92fb830edea75f804f89
Girish Suthar <102278281+girishsutar229@users.noreply.github.com>
2023-08-03 01:30:08 +0530

    Time: 6 ms (99.77%) | Memory: 16.9 MB (62.09%) - LeetSync

---
+++ 136-single-number/single-number.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        //size of the array:
+        int n = nums.size();
+
+       // XOR all the elements:
+       int xorr = 0;
+       for (int i = 0; i < n; i++) {
+         xorr = xorr ^ nums[i];
+       }
+    return xorr;
+
+
+//second type
+        /*
+        int n = nums.size();
+        // Using map to keep track of Frequency of element.
+        map<int,int> frequency;
+
+        // Iterating over array to add count of element to map.
+        for(int i=0;i<n;i++){
+            frequency[nums[i]]++; //frequency[key]++
+            //here we are at key nums[i] incrementing it value by 1.
+        }
+
+        // Ans variable to return.
+        int ans;
+
+        // Here we are cheking in whole map that if anyone's count is 1 means second value is 1 
+        //then return it's first means that element.
+        for(auto it = frequency.begin(); it!=frequency.end();it++){
+            if(it->second == 1){
+                ans = it->first;
+            }
+        }
+
+        return ans;
+
+
+        // We can use XOR also for this problem
+    }
+        */
+        
+    }
+};
